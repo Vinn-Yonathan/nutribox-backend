7df1a13f3b787a36aed5e552136f02c43984e98c
Cevin Albert Yonathan <126557855+Vinn-Yonathan@users.noreply.github.com>
2026-04-15 12:10:58 +0700

    Change Laravel server port from 8000 to 8080

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 php artisan migrate --force
 
 echo "Starting Laravel application..."
-php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
+php artisan serve --host=0.0.0.0 --port="${PORT:-8080}"
