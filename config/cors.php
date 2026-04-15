2c6fd6fa8d00169a4f70d28aff934b6c0cdb70a9
Cevin Albert Yonathan <cevinalbertyonathan@gmail.com>
2026-04-15 11:26:09 +0700

    feat(cors): add new allowed origins

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://nutribox-dummy.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
